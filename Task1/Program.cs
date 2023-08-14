9b17a567d8089c60f02dca6aaa672040d0b9ea26
Tatiana <tatianka.dmitrieva.96@mail.ru>
2023-08-14 23:49:35 +0300

    SecondDigit

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 10: Напишите программу, 
+которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+using System.Globalization;
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int n = int.Parse(Console.ReadLine()!);
+int second = n / 10 % 10;
+Console.WriteLine ($"Вторая цифра в цисле {n} равна {second}");
